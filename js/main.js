9d061a0e90df882661d59070bd397ac3b28a89a8
이소윤 <s_lee77176@fanshaweonline.ca>
2020-02-11 06:53:12 -0500

    stylized done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import MainComponent from "./modules/MainComponent.js";
 import ContactComponent from "./modules/ContactComponent.js";
-import ErrorComponent from "./modules/ErrorComponent.js"
+import ErrorComponent from "./modules/ErrorComponent.js";
 
 const routes = [
    { path: '/', name: 'Main', component: MainComponent },
@@ -18,37 +18,16 @@ const router = new VueRouter({
   const vm = new Vue({
     data: {
 
-      cityimage:"",
-      cityname:"",
-      cityinfo:""
+      
 
 
     },
 
-    method: {
-      fetchUsers() {
-        // get our user data here and push it back into the VM
-        console.log('fetch user data here');
-        
-        const url = './includes/index.php?user=true';
-        
-        // the AJAX part
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-  
-          // push our user data into the VM
-          this.user = data[0];
-        })
-        .catch((err) => console.log(err))
-      
-      }
+    methods: {
+     
 
 
     },
 
     router
-
-
 }).$mount("#app");
--- js/modules/MainComponent.js
+++ js/modules/MainComponent.js
@@ -1,18 +1,30 @@
 export default {
     
-    template: `
-    <h1>{{ message }}</h1>`,
+    template: `<footer id="main_Footer">
+    <div class="footer-left">
+        <h1>FOLLOW US</h1>
+            <p>Terms of Service</p>
+            <p>Privacy</p>
+    </div>
+        <ul>
+                <li class="fab fa-facebook-square"></i></li>
+                <li class="fab fa-instagram"></i></li>
+                <li class="fab fa-twitter-square"></i></li>
+        </ul>
+</footer>`,
    
     created: function() {
         console.log('our  main component rendered');
         
+    
     },
-   
-    data: function() {
-        return {
-            message: "Welcome to my Super Awesome Soyoon's App!"
-        }
+
+    methods:{
+        
+
     }
+
+    
 
 
    
